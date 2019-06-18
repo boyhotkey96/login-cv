b9d8714a69ffa5b6e970a02d09337d9ccf2c1e49
boyhotkey96 <boyhotkey96@gmail.com>
2019-06-18 07:30:03 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# login-cv-PhungBaDu
---
+++ js/login.js
@@ -0,0 +1,9 @@
+function login() {
+	var mail = document.getElementById("mail").value;
+	var pass = document.getElementById("pass").value;	
+	if (mail == "vexere" && pass == "vxr2019") {
+		 window.location="cv.html";
+	} else {
+		alert("Username or Password không đúng!");
+	}
+}
