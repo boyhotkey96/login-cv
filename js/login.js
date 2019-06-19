c5136f362d91d8f156e5791408365b8f81af9a59
boyhotkey96 <boyhotkey96@gmail.com>
2019-06-19 18:19:07 +0700

    bai1

--- js/login.js
+++ js/login.js
@@ -2,7 +2,7 @@ function login() {
 	var mail = document.getElementById("mail").value;
 	var pass = document.getElementById("pass").value;	
 	if (mail == "vexere" && pass == "vxr2019") {
-		 window.location="cv.html";
+		 window.location = "cv.html";
 	} else {
 		alert("Username or Password không đúng!");
 	}
